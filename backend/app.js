82f218fda4fec8d3174e7be902f80c95ec8bcbd7
ChristianPrieto243 <cprieto2023@my.fit.edu>
2024-11-29 09:17:28 -0500

    Update app.js
    field name on register to password
    field name in favorite changed to favorited_movies
    authentication check
    get to post and get data from req.body
    corrected password comparison

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 // run "node server.js" to start
+// backend/app.js
 
 const express = require('express');
 const app = express();
@@ -31,19 +32,21 @@ db.on("error", function (err) {
 });
 
 // routes
+
 // Register
 app.post("/register", async (req, res) => {
     try {
         const { first_name, last_name, email, password } = req.body;
         const salt = await bcrypt.genSalt(10);
-        hashed_password = await bcrypt.hash(password, salt);
+        const hashed_password = await bcrypt.hash(password, salt);
         const user = new User({
             first_name,
             last_name,
             email,
-            hashed_password
-        })
+            password: hashed_password  //Corrected field name to 'password'
+        });
         await user.save();
+        res.send({ message: "Registration successful" });
     } catch (error) {
         res.send({ error: error.message });
     }
@@ -52,8 +55,16 @@ app.post("/register", async (req, res) => {
 // Favorite a movie
 app.post("/favorite", async (req, res) => {
     try {
-        const {email, title} = req.body;
-        await User.findOneAndUpdate({ email: email}, {$addToSet:{ title: title }});
+        const email = req.cookies["session-cookie"];
+        if (!email) {
+            return res.status(401).send({ error: "Not authenticated" });
+        }
+        const { title } = req.body;
+        await User.findOneAndUpdate(
+            { email },
+            { $addToSet: { favorited_movies: title } }  //Corrected field name to 'favorited_movies'
+        );
+        res.send({ message: "Movie added to favorites" });
     } catch (error) {
         res.send({ error: error.message });
     }
@@ -63,7 +74,7 @@ app.post("/favorite", async (req, res) => {
 app.post("/genres", async (req, res) => {
     try {
         const genre = req.body.genre;
-        const movies = await Movie.find({"info.genres": genre}).sort({"info.rating":-1});
+        const movies = await Movie.find({ "info.genres": genre }).sort({ "info.rating": -1 });
         res.send(movies);
     } catch (error) {
         res.send({ error: error.message });
@@ -80,28 +91,40 @@ app.get("/genrelist", async (req, res) => {
     }
 });
 
-// Return movies that the currently logged-in user has favorited.  The email is in the cookie that is set
+// Return movies that the currently logged-in user has favorited
 app.get("/favorited", async (req, res) => {
     try {
-        const email = req.body.email;
-        const user = await User.findOne({email});
-        res.send(user.liked_movies);
+        const email = req.cookies["session-cookie"];
+        if (!email) {
+            return res.status(401).send({ error: "Not authenticated" });  //Added authentication check
+        }
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).send({ error: "User not found" });
+        }
+        const favoriteTitles = user.favorited_movies;
+        // Retrieve movie details for these titles
+        const movies = await Movie.find({ title: { $in: favoriteTitles } });
+        res.send(movies);
     } catch (error) {
         res.send({ error: error.message });
     }
 });
 
-// Sign in.  Also sets cookies
-app.get("/signin", async (req, res) => {
+// Sign in. Also sets cookies
+app.post("/signin", async (req, res) => {
     try {
-        const { email, password } = req.body;
-        const user = await User.findOne({email});
-        const isCorrectPassword = await bcrypt.compare(password, user.password);
+        const { email, password } = req.body;  //Changed from GET to POST and get data from req.body
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(401).send({ message: "User not found" });
+        }
+        const isCorrectPassword = await bcrypt.compare(password, user.password);  //Corrected password comparison
         if (isCorrectPassword) {
-            res.send(email);
             res.cookie("session-cookie", email);
+            res.send({ message: "Login successful" });
         } else {
-            res.send({message: "Incorrect Password"});
+            res.status(401).send({ message: "Incorrect Password" });
         }
     } catch (error) {
         res.send({ error: error.message });
@@ -112,7 +135,7 @@ app.get("/signin", async (req, res) => {
 app.get("/signout", (req, res) => {
     try {
         res.clearCookie("session-cookie");
-        res.send({message: "logged out"});
+        res.send({ message: "Logged out" });
     } catch (error) {
         res.send({ error: error.message });
     }
