0ae7dbac014f1822ab0b2378480a62d578d29c4d
ChristianPrieto243 <cprieto2023@my.fit.edu>
2024-11-29 19:51:25 -0500

    Update Movies.js isFavorited prop to the moviecard

--- frontend/src/components/Movies.js
+++ frontend/src/components/Movies.js
@@ -1,46 +1,90 @@
+// frontend/src/components/Movies.js
+
 import React, { useState, useEffect } from 'react';
 import MovieCard from './MovieCard';
 
-function MovieRow({genre_search}){
-    const [movies, setMovies] = useState([]);
-    const formData = {
-        genre: genre_search
-    };
-    useEffect(() => {
-        fetch("http://localhost:3000/genres", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(formData),
-        }).then(res => res.json()).then((mv) => setMovies(mv.slice(0, 10)));
-      }, []);
+function MovieRow({ genre_search, favoritedMovies, refreshFavorites }) {
+  const [movies, setMovies] = useState([]);
+  const formData = {
+    genre: genre_search
+  };
 
-    return(                    
-        <div className="movie-cards">
-            {movies.map(movie => (
-                <MovieCard title={movie.title} rating={movie.info.rating} duration={movie.info.running_time_secs} imageUrl={movie.info.image_url}/>)
-            )}
-        </div>
-    )
+  useEffect(() => {
+    fetch("/genres", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(formData),
+    }).then(res => res.json()).then((mv) => setMovies(mv.slice(0, 10)));
+  }, [genre_search]);
+
+  return (
+    <div className="movie-cards">
+      {movies.map(movie => (
+        <MovieCard
+          key={movie._id}
+          title={movie.title}
+          rating={movie.info.rating}
+          duration={movie.info.running_time_secs}
+          imageUrl={movie.info.image_url}
+          isFavorited={favoritedMovies.includes(movie.title)}
+          onFavoriteToggle={refreshFavorites}
+        />
+      ))}
+    </div>
+  );
 }
 
-function Movies(){
-    const [genres, setGenres] = useState([]);
-    useEffect(() => {
-        fetch("http://localhost:3000/genrelist", {
-            method: "GET",
-            headers: { "Content-Type": "application/json" },
-        }).then(res => res.json()).then((glist) => setGenres(glist)).catch((e) => console.log(e));
-      }, []);
-    return(
-        <div className = "movies">
-            {genres.map((genre_name) => (
-                <section className="genre-section">
-                    <h2>{genre_name}</h2>
-                    <MovieRow genre_search={genre_name}/>
-                </section>
-            ))}
-        </div>
-    );
+function Movies() {
+  const [genres, setGenres] = useState([]);
+  const [favoritedMovies, setFavoritedMovies] = useState([]);
+
+  const fetchFavoritedMovies = () => {
+    fetch('/favorited', {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+      credentials: 'include',
+    })
+      .then(res => {
+        if (res.status === 401) {
+          setFavoritedMovies([]);
+          return [];
+        }
+        return res.json();
+      })
+      .then(data => {
+        if (Array.isArray(data)) {
+          const titles = data.map(movie => movie.title);
+          setFavoritedMovies(titles);
+        }
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+  useEffect(() => {
+    fetch("/genrelist", {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    }).then(res => res.json()).then((glist) => setGenres(glist)).catch((e) => console.log(e));
+
+    fetchFavoritedMovies();
+  }, []);
+
+  return (
+    <div className="movies">
+      {genres.map((genre_name) => (
+        <section className="genre-section" key={genre_name}>
+          <h2>{genre_name}</h2>
+          <MovieRow
+            genre_search={genre_name}
+            favoritedMovies={favoritedMovies}
+            refreshFavorites={fetchFavoritedMovies}
+          />
+        </section>
+      ))}
+    </div>
+  );
 }
 
 export default Movies;
