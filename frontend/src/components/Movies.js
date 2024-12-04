d23e75c2c18226526c773edad310d773f4b3d634
gruth1017 <142533806+gruth1017@users.noreply.github.com>
2024-12-03 23:29:27 -0500

    Fix horizontal scroll and add left-right navigation for movie row

--- frontend/src/components/Movies.js
+++ frontend/src/components/Movies.js
@@ -1,6 +1,6 @@
 // frontend/src/components/Movies.js
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import MovieCard from './MovieCard';
 
 function MovieRow({ genre_search, favoritedMovies, refreshFavorites }) {
@@ -8,6 +8,9 @@ function MovieRow({ genre_search, favoritedMovies, refreshFavorites }) {
   const formData = {
     genre: genre_search
   };
+
+  // reference for the move row container
+  const movieRowRef = useRef(null); 
 
   useEffect(() => {
     fetch("/genres", {
@@ -17,19 +20,54 @@ function MovieRow({ genre_search, favoritedMovies, refreshFavorites }) {
     }).then(res => res.json()).then((mv) => setMovies(mv.slice(0, 10)));
   }, [genre_search]);
 
+  // Scroll left
+  const scrollLeft = () => {
+    if (movieRowRef.curent) {
+      movieRowRef.current.scrollBy({
+        // Scroll 200px left
+        left: -200,
+        behavior: 'smooth'
+      });
+    }
+  };
+
+  // Scroll right
+  const scrollRight = () => {
+    if (movieRowRef.current) {
+      movieRowRef.current.scrollBy({
+        // Scroll 200px right
+        left: 200,
+        behavior: 'smooth'
+      });
+    }
+  };
+
+
   return (
-    <div className="movie-cards">
-      {movies.map(movie => (
-        <MovieCard
-          key={movie._id}
-          title={movie.title}
-          rating={movie.info.rating}
-          duration={movie.info.running_time_secs}
-          imageUrl={movie.info.image_url}
-          isFavorited={favoritedMovies.includes(movie.title)}
-          onFavoriteToggle={refreshFavorites}
-        />
-      ))}
+    <div className="movie-row-container" style={{ position: 'relative' }}>
+      {/* Left Arrow */}
+      <div className="carousel-arrows left-arrow" onClick={scrollLeft}>
+        &lt;
+      </div>
+
+      <div className="movie-cards" ref={movieRowRef}>
+        {movies.map(movie => (
+          <MovieCard
+            key={movie._id}
+            title={movie.title}
+            rating={movie.info.rating}
+            duration={movie.info.running_time_secs}
+            imageUrl={movie.info.image_url}
+            isFavorited={favoritedMovies.includes(movie.title)}
+            onFavoriteToggle={refreshFavorites}
+          />
+        ))}
+      </div>
+
+      {/* Right Arrow */}
+      <div className="carousel-arrows right-arrow" onClick={scrollRight}>
+        &gt;
+      </div>
     </div>
   );
 }
